fc561c0f49a59a4de1d2bf8ce9c8f4fff1f49c21
yuuhikaze <yuuhikaze@protonmail.com>
2026-01-24 18:42:28 -0500

    feat: adds flake nix with develop shell

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  description = "Tern: Modular batch conversion interface";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rustVersion;
+          cargo = rustVersion;
+        };
+
+        tern = rustPlatform.buildRustPackage {
+          pname = "tern";
+          version = "1.6.6";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            rustVersion
+            cmake
+          ];
+
+          buildInputs = with pkgs; [
+            lua5_4
+            sqlite
+            openssl
+            fontconfig
+            wayland
+            libxkbcommon
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+          ];
+
+          # Slint needs some environment variables during build if it can't find libraries
+          # We might need to set SLINT_STYLE or similar if required.
+        };
+      in
+      {
+        packages.default = tern;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustVersion
+            pkgs.pkg-config
+            pkgs.lua5_4
+            pkgs.sqlite
+            pkgs.openssl
+            pkgs.cmake
+          ] ++ (with pkgs; [
+            fontconfig
+            wayland
+            libxkbcommon
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+          ]);
+
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (with pkgs; [
+              wayland
+              libxkbcommon
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXrandr
+              xorg.libXi
+              fontconfig
+            ])}
+          '';
+        };
+      }
+    );
+}
